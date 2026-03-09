4c5962817a1a02f1a34037b6a444dcab34855a6a
RaqeebDev <raqeebauto@gmail.com>
2026-03-09 22:07:47 +0530

    route message

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,29 +1,17 @@
 import express from "express";
+import { getAllContacts } from "../controllers/message.controller.js";
+import {protectRoute} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/contacts", getAllContacts);
+
+router.get("/contacts",protectRoute, getAllContacts);
 // router.get("/chats", getChatPartners);
 // router.get("/:id", getMessagesByUserId);
+
 // router.post("/send/:id", sendMessage);
 
 export default router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
