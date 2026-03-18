5e33d5984d89007d15838d357d7ee0f0e8be2f24
RaqeebDev <raqeebauto@gmail.com>
2026-03-18 07:30:38 +0530

    form: left bar

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuthStore } from "../store/useAuthStore.js";
+import BorderAnimatedContainer from "../components/borderAnimatedContainer.jsx";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -13,7 +14,20 @@ const SignUpPage = () => {
 
   return (
     <div className="w-full flex items-center justify-center p-4 bg-slate-900">
-      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px] "></div>
+      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px] ">
+        <BorderAnimatedContainer>
+          <div className="w-full flex flex-col md:flex-row">
+            {/* form - left  */}
+            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30  ">
+            <div className="w-full max-w-md">
+
+            </div>
+            </div>
+
+
+          </div>
+        </BorderAnimatedContainer>
+      </div>
     </div>
   );
 };
