67f949136600208a647a366be1b5c5ea37690ff9
RaqeebDev <raqeebauto@gmail.com>
2026-03-18 07:24:14 +0530

    component: animated border

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,12 +43,7 @@ function App() {
         />
       </Routes>
 
-      <button
-        className="relative z-50 bg-black text-white px-4 py-2 rounded"
-        onClick={() => toast.success("It")}
-      >
-        Click Me
-      </button>
+    
 
       <Toaster />
     </div>
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -9,9 +9,13 @@ const SignUpPage = () => {
   });
   const { signUp, isSigningUp } = useAuthStore();
 
-  // return (
+  const handleSubmit = (e) => {};
 
-  // )
+  return (
+    <div className="w-full flex items-center justify-center p-4 bg-slate-900">
+      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px] "></div>
+    </div>
+  );
 };
 
 export default SignUpPage;
