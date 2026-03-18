589e212a1b19e01076134ad8ca85128d0f60436e
RaqeebDev <raqeebauto@gmail.com>
2026-03-18 10:39:15 +0530

    fixed axios url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: (import.meta.env.MODE = "development"
-    ? "http://localhost:5173/api"
-    : "https://light-8bd4.onrender.com/api"),
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5173/api"
+      : "https://light-8bd4.onrender.com/api",
   withCredentials: true,
 });
