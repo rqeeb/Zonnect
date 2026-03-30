0ac02ce8fd6f1e5fedf1754e6b048cef0ed378f8
RaqeebDev <raqeebauto@gmail.com>
2026-03-30 16:19:58 +0530

    UsersLoadingSkeleton & chatList update

--- frontend/src/components/ChatsList.jsx
+++ frontend/src/components/ChatsList.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function ChatsList() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
 
-export default ChatsList
+export default ChatsList;
