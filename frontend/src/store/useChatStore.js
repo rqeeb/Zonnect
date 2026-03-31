9492324afc8d8051bb1e534cacf30ce71d2a27bd
RaqeebDev <raqeebauto@gmail.com>
2026-03-31 21:21:12 +0530

    get messages store - place holder: no conveos found

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -18,7 +18,7 @@ export const useChatStore = create((set, get) => ({
     set({ isSoundEnabled: !get().isSoundEnabled });
   },
 
-   setActiveTab: (tab) => set({ activeTab: tab }),
+  setActiveTab: (tab) => set({ activeTab: tab }),
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 
   getAllContacts: async () => {
@@ -43,6 +43,16 @@ export const useChatStore = create((set, get) => ({
       set({ isUsersLoading: false });
     }
   },
-
+  getMessagesByUserId: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Something went wrong");
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
   
 }));
